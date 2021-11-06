f93a8a0cfac387bee49d48817fe3e3f06e356288
MichaelSchwabe <michael_schwabe@mailbox.org>
2021-11-06 20:40:00 +0100

    initial structure of util script with first methodhead deklaration #5

---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+
+
+def transform_zipcodes_to_geometries(df):
+    '''
+    Parameters:
+    -----------
+    df: pandas.DataFrame
+        DataFrame with ID, ZipCodes and Inhabitant number 
+
+    Retruns:
+    -----------
+    zip_and_geom: GeoJson
+        with Geometry Object for every Zipcode
+    '''
+    
+
+
+
+
+    pass
