28316689cb64ccf489e6dc681a4c6bb423680ed0
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-12-10 19:48:36 +0100

    feat: add REST endpoint for metrics #24

---
+++ services/metrics/app/app.py
@@ -0,0 +1,40 @@
+import random
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:4200",
+        "https://openpublictransport.de",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class PointOnInterest(BaseModel):
+    lat = 0
+    lon = 0
+
+
+class Metrics:
+    index = 0
+
+    def __init__(self, index):
+        self.index = index
+
+
+@app.post("/index")
+def get_mobility_index(data: PointOnInterest):
+    data = data.dict()
+    lat = data["lat"]
+    lon = data["lon"]
+
+    return Metrics(
+        index=random.randint(0, 100)
+    )
