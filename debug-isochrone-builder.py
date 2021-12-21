456d7d3f437b979af8ef61510c3b14d161778e92
Florian Schwanz <florian.schwanz2@de.bosch.com>
2021-12-21 21:04:17 +0100

    fix: isochrone calculation #35

---
+++ debug-isochrone-builder.py
@@ -0,0 +1,113 @@
+import os
+import sys
+
+file_path = os.path.realpath(__file__)
+script_path = os.path.dirname(file_path)
+
+# Make library available in path
+library_paths = [
+    os.path.join(script_path, "lib"),
+    os.path.join(script_path, "lib", "cloud"),
+    os.path.join(script_path, "lib", "loader", "osmnx"),
+    os.path.join(script_path, "lib", "loader", "overpass"),
+    os.path.join(script_path, "lib", "loader", "peartree"),
+    os.path.join(script_path, "lib", "converter"),
+    os.path.join(script_path, "lib", "log"),
+]
+
+for p in library_paths:
+    if not (p in sys.path):
+        sys.path.insert(0, p)
+
+# Import library classes
+from tracking_decorator import TrackingDecorator
+from peartree_graph_loader import PeartreeGraphLoader
+from logger_facade import LoggerFacade
+from isochrone_builder import IsochroneBuilder
+
+
+#
+# Main
+#
+
+@TrackingDecorator.track_time
+def main(argv):
+    # Set default values
+    clean = False
+    quiet = False
+    points_per_sqkm = 100
+    cities = [
+        {"name": "berlin", "query": "Berlin, Germany", "area": 891, "inhabitants": 3_600_000,
+         "bounding_box": [13.088333218007715, 52.33824183586156, 13.759587218876971, 52.67491714954712],
+         "transport_association": "vbb"}
+    ]
+    places = [
+        {"name": "gesundbrunnen", "coords": (13.389444444444, 52.548611111111)},
+        {"name": "ahrensfelde", "coords": (13.565, 52.571667)},
+        {"name": "hauptbahnhof", "coords": (13.369444, 52.525)}
+    ]
+    start_end_times = [(7 * 60 * 60, 8 * 60 * 60)]
+    travel_times = [15]
+
+    # Set paths
+    data_path = os.path.join(script_path, "data", "data")
+    base_results_path = os.path.join(script_path, "results", "debug")
+
+    # Iterate over cities
+    for city in cities:
+
+        city_name = city["name"]
+        query = city["query"]
+        area = city["area"]
+        inhabitants = city["inhabitants"]
+        bounding_box = city["bounding_box"]
+        transport_association = city["transport_association"]
+
+        results_path = os.path.join(base_results_path, city_name)
+
+        # Initialize logger
+        logger = LoggerFacade(results_path, console=True, file=True)
+
+        # Iterate over start/end times
+        for times in start_end_times:
+            start_time = times[0]
+            end_time = times[1]
+
+            # Load transport graph
+            graph = PeartreeGraphLoader().run(
+                logger=logger,
+                data_path=data_path,
+                results_path=os.path.join(results_path, "graphs", "peartree"),
+                transport_association=transport_association,
+                start_time=start_time,
+                end_time=end_time,
+                existing_graph=None,
+                clean=True,
+                quiet=quiet
+            )
+
+            # Iterate over travel times
+            for travel_time in travel_times:
+
+                # Iterate over places
+                for place in places:
+                    place_name = place["name"]
+                    coords = place["coords"]
+                    results_path = os.path.join(base_results_path, place_name)
+
+                    # Build isochrone
+                    IsochroneBuilder().run_for_place(
+                        logger=logger,
+                        data_path=data_path,
+                        results_path=os.path.join(results_path, "geojson"),
+                        city=city_name,
+                        graph=graph,
+                        travel_time=travel_time,
+                        place=coords,
+                        clean=clean,
+                        quiet=quiet
+                    )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- lib/isochrone_builder.py
+++ lib/isochrone_builder.py
@@ -13,15 +13,23 @@ from tqdm import tqdm
 from tracking_decorator import TrackingDecorator
 
 
-def get_spatial_distance(logger, data_path, city, graph, start_point, travel_time, distance_attribute="time"):
+def get_isochrone(logger, graph, start_point, travel_time, distance_attribute="time"):
     try:
-        nodes, edges, walking_distance_meters = get_possible_routes(
+        return get_possible_routes(
             graph=graph,
             start_point=start_point,
             travel_time=travel_time,
             distance_attribute=distance_attribute,
             calculate_walking_distance=True
         )
+    except Exception as e:
+        logger.log_line(f"✗️ Exception: {str(e)}")
+        return None, 0
+
+
+def get_isochrone_metrics(logger, data_path, city, start_point, subgraph, walking_distance_meters):
+    try:
+        nodes, edges = ox.graph_to_gdfs(subgraph)
 
         # Define valid polygons
         polygon_file = os.path.join(data_path, "cities", city, "boundaries", "boundaries.geojson")
@@ -29,7 +37,7 @@ def get_spatial_distance(logger, data_path, city, graph, start_point, travel_tim
 
         longitudes, latitudes = get_convex_hull(nodes=nodes)
         points = []
-        for point in zip(latitudes, longitudes):
+        for point in zip(longitudes, latitudes):
 
             p = ogr.Geometry(ogr.wkbPoint)
             p.AddPoint(point[0], point[1])
@@ -37,6 +45,8 @@ def get_spatial_distance(logger, data_path, city, graph, start_point, travel_tim
             # Check if point is within city limits
             if is_in_desired_area(p, valid_polygons):
                 points.append([point[0], point[1]])
+            else:
+                logger.log_line(f"{city} point outside city {point[0]}, {point[1]}")
 
         convex_hull_polygon = Polygon(points)
 
@@ -102,11 +112,9 @@ def get_possible_routes(graph, start_point, travel_time, distance_attribute, cal
         radius = travel_time
 
     if radius > 0:
-        subgraph = nx.ego_graph(graph, center_node, radius=radius, distance=distance_attribute)
-        nodes, edges = ox.graph_to_gdfs(subgraph)
-        return nodes, edges, walking_distance_meters
+        return nx.ego_graph(graph, center_node, radius=radius, distance=distance_attribute), walking_distance_meters
     else:
-        return [], [], walking_distance_meters
+        return None, walking_distance_meters
 
 
 def get_convex_hull(nodes):
@@ -143,6 +151,43 @@ def write_points_to_geojson(file_path, coords, travel_time):
         f.write("%s" % collection)
 
 
+def write_nodes_to_geojson(file_path, graph):
+    features = []
+
+    if len(graph.nodes) > 0:
+        for node_id in graph.nodes:
+            node = graph.nodes[node_id]
+            feature = {}
+            feature["geometry"] = {"type": "Point", "coordinates": [node["x"], node["y"]]}
+            feature["type"] = "Feature"
+            features.append(feature)
+
+    collection = FeatureCollection(features)
+
+    with open(file_path, "w") as f:
+        f.write("%s" % collection)
+
+
+def write_polygon_to_geojson(file_path, graph):
+    features = []
+    coordinates = []
+
+    for node_id in graph.nodes:
+        node = graph.nodes[node_id]
+        coordinates.append([node["x"], node["y"]])
+
+    if len(graph.nodes) > 0:
+        feature = {}
+        feature["geometry"] = {"type": "Polygon", "coordinates": coordinates}
+        feature["type"] = "Feature"
+        features.append(feature)
+
+    collection = FeatureCollection(features)
+
+    with open(file_path, "w") as f:
+        f.write("%s" % collection)
+
+
 #
 # Main
 #
@@ -164,17 +209,22 @@ class IsochroneBuilder:
             point = sample_points[point_index]
             start_point = (float(point["lat"]), float(point["lon"]))
 
-            mean_spatial_distance, \
-            median_spatial_distance, \
-            min_spatial_distance, \
-            max_spatial_distance, \
-            area = get_spatial_distance(
+            # Calculate isochrone
+            subgraph, walking_distance_meters = get_isochrone(
                 logger=logger,
-                data_path=data_path,
-                city=city,
                 graph=graph,
                 start_point=start_point,
                 travel_time=travel_time
+            )
+
+            # Determine isochrone metrics
+            mean_spatial_distance, median_spatial_distance, min_spatial_distance, max_spatial_distance, area = get_isochrone_metrics(
+                logger=logger,
+                data_path=data_path,
+                city=city,
+                start_point=start_point,
+                subgraph=subgraph,
+                walking_distance_meters=walking_distance_meters
             )
 
             point_with_spatial_distance = {
@@ -191,6 +241,65 @@ class IsochroneBuilder:
                 points_with_spatial_distance.append(point_with_spatial_distance)
             else:
                 failed_points.append(point_with_spatial_distance)
+
+        write_points_to_geojson(
+            file_path=os.path.join(results_path, "isochrones-" + str(travel_time) + ".geojson"),
+            coords=points_with_spatial_distance,
+            travel_time=travel_time
+        )
+
+        write_points_to_geojson(
+            file_path=os.path.join(results_path, "isochrones-" + str(travel_time)) + "-failed.geojson",
+            coords=failed_points,
+            travel_time=travel_time
+        )
+
+        return points_with_spatial_distance, failed_points
+
+    @TrackingDecorator.track_time
+    def run_for_place(self, logger, data_path, results_path, city, graph, travel_time, place, clean=False, quiet=False):
+        points_with_spatial_distance = []
+        failed_points = []
+        start_point = (place[1], place[0])
+
+        # Make results path
+        os.makedirs(os.path.join(results_path), exist_ok=True)
+
+        # Calculate isochrone
+        subgraph, walking_distance_meters = get_isochrone(
+            logger=logger,
+            graph=graph,
+            start_point=start_point,
+            travel_time=travel_time
+        )
+
+        # Determine isochrone metrics
+        mean_spatial_distance, median_spatial_distance, min_spatial_distance, max_spatial_distance, area = get_isochrone_metrics(
+            logger=logger,
+            data_path=data_path,
+            city=city,
+            start_point=start_point,
+            subgraph=subgraph,
+            walking_distance_meters=walking_distance_meters
+        )
+
+        write_nodes_to_geojson(file_path=os.path.join(results_path, "isochrone-nodes-" + str(travel_time) + ".geojson"), graph=subgraph)
+        write_polygon_to_geojson(file_path=os.path.join(results_path, "isochrone-hull-" + str(travel_time) + ".geojson"), graph=subgraph)
+
+        point_with_spatial_distance = {
+            "lon": start_point[1],
+            "lat": start_point[0],
+            "mean_spatial_distance_" + str(travel_time) + "min": mean_spatial_distance,
+            "median_spatial_distance_" + str(travel_time) + "min": median_spatial_distance,
+            "min_spatial_distance_" + str(travel_time) + "min": min_spatial_distance,
+            "max_spatial_distance_" + str(travel_time) + "min": max_spatial_distance,
+            "area_" + str(travel_time) + "min": area
+        }
+
+        if mean_spatial_distance > 0:
+            points_with_spatial_distance.append(point_with_spatial_distance)
+        else:
+            failed_points.append(point_with_spatial_distance)
 
         write_points_to_geojson(
             file_path=os.path.join(results_path, "isochrones-" + str(travel_time) + ".geojson"),
