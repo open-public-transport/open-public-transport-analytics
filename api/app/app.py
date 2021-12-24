b48e5a2c3483c20556c7b8820800a56867e4e703
Florian Schwanz <florian.schwanz2@de.bosch.com>
2021-12-24 11:04:36 +0100

    feat: add random sample values to endpoint

--- api/app/app.py
+++ api/app/app.py
@@ -29,7 +29,7 @@ class CityBasicInformation:
 
 class RankedValue:
     def __init__(self):
-        self.raw_value = random.randint(0, 24)
+        self.raw_value = random.randint(1, 24)
         self.overall_rank = -1  # Rank across all cities
         self.grouped_rank = -1  # Rank across cities of same group, e.g. metropolis, small town
         self.overall_percentile = -1  # Percentile across all cities
