0d64e1f53c1d26cdd471cb50a8c3a1fc17f0dcb7
Florian Schwanz <florian.schwanz2@de.bosch.com>
2022-01-01 23:10:43 +0100

    feat: determine lines by station IDs #16

--- lib/metrics/place_metrics_builder.py
+++ lib/metrics/place_metrics_builder.py
@@ -5,6 +5,7 @@ from bike_information import BikeInformation
 from cities import Cities
 from line_information import LineInformation
 from overpass_station_loader import OverpassStationLoader
+from overpass_route_loader import OverpassRouteLoader
 from place_metrics import PlaceMetrics
 from ranked_value import RankedValue
 from station_information import StationInformation
@@ -34,6 +35,41 @@ def get_station_information(results_path, city_name, bounding_box, public_transp
     station_information.absolute_stations_count = absolute_stations_count
 
     return station_information
+
+def get_line_information(results_path, city_name, bounding_box, public_transport_type, lat, lon):
+    stations = OverpassStationLoader().run(
+        logger=None,
+        results_path=os.path.join(results_path, "osm"),
+        city_id=city_name,
+        bounding_box=bounding_box,
+        public_transport_type=public_transport_type,
+        clean=False,
+        quiet=True
+    )
+    routes = OverpassRouteLoader().run(
+        logger=None,
+        results_path=os.path.join(results_path, "osm"),
+        city_id=city_name,
+        bounding_box=bounding_box,
+        public_transport_type=public_transport_type,
+        clean=False,
+        quiet=True
+    )
+
+    walking_time_min = 15
+    walking_speed_kph = 4.5
+    radius_km = walking_time_min / 60 * walking_speed_kph
+
+    station_ids = get_stations_in_radius(lat, lon, radius_km, stations)
+    line_ids = get_lines_by_stations(routes, station_ids)
+
+    absolute_lines_count = RankedValue()
+    absolute_lines_count.raw_value = len(line_ids)
+
+    line_information = LineInformation(public_transport_type=public_transport_type)
+    line_information.absolute_lines_count = absolute_lines_count
+
+    return line_information
 
 
 # See https://en.wikipedia.org/wiki/Haversine_formula
@@ -73,6 +109,23 @@ def get_stations_in_radius(lat, lon, radius, stations):
     return nodes
 
 
+def get_lines_by_stations(routes, station_ids):
+    lines = []
+
+    if routes is not None:
+        elements = routes["elements"]
+        for element in elements:
+            id = element["id"]
+            members = element["members"]
+
+            for member in members:
+                role = member["role"]
+                ref = member["ref"]
+                if role == "stop" and ref in station_ids and id not in lines:
+                    lines.append(id)
+
+    return lines
+
 class PlaceMetricsBuilder:
 
     def __init__(self, results_path):
@@ -90,8 +143,7 @@ class PlaceMetricsBuilder:
 
         for public_transport_type in public_transport_types:
             transport_station_information = get_station_information(self.results_path, city_name, bounding_box, public_transport_type=public_transport_type, lat=lat, lon=lon)
-            # TODO
-            transport_line_information = LineInformation(public_transport_type=public_transport_type),
+            transport_line_information = get_line_information(self.results_path, city_name, bounding_box, public_transport_type=public_transport_type, lat=lat, lon=lon)
 
             if transport_station_information is not None:
                 station_information.append(transport_station_information)
